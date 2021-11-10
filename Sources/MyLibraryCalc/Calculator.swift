d4067fcf1192ce5157dbee2fc0b50639e184c955
Vinuraj M V <vinuraj.m.v@walmart.com>
2021-11-10 15:14:40 +0530

    first commit

---
+++ Sources/MyLibraryCalc/Calculator.swift
@@ -0,0 +1,20 @@
+//
+//  Calculator.swift
+//  CalculatorA
+//
+//  Created by Vinuraj M v on 09/11/21.
+//
+
+import Foundation
+
+public class Calculator {
+    
+    public init() {
+        
+    }
+    
+    public func mutiply(value1: Int, value2: Int) -> Int {
+        
+        return value1 * value2
+    }
+}
